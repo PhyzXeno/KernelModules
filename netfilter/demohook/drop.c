d9ae83fddd70ed61d1a5905e53de9a7b89a45f2b
phyzx92 <phyzx92@bupt.edu.cn>
2017-08-24 10:50:19 +0800

    finally worked
    新版的hook函数的参数有变化，在写函数的时候要跟着新版本走

--- netfilter/demohook/drop.c
+++ netfilter/demohook/drop.c
@@ -1,5 +1,9 @@
-// #define __KERNEL__
-// #define MODULE
+#ifndef __KERNEL__
+    #define __KERNEL__
+#endif
+#ifndef MODULE
+    #define MODULE
+#endif
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/netfilter.h>
@@ -8,27 +12,48 @@
 static struct nf_hook_ops netfilter_ops_in;
 static struct nf_hook_ops netfilter_ops_out;
 
+// unsigned int main_hook(
+//     unsigned int hooknum,
+//     struct sk_buff **skb,
+//     const struct net_device *in,
+//     const struct net_device *out,
+//     int (*okfn)(struct sk_buff*)
+// )
+// {
+//     printk(KERN_INFO "Packet Dropped.");
+//     return NF_DROP;
+// }
+
+/*
+In the (IMHO) latest (released) netfilter version, nf_hookfn (the base type of nf_hook_ops.hook) is defined as follows:
+
+typedef unsigned int nf_hookfn(void *priv,
+                   struct sk_buff *skb,
+                   const struct nf_hook_state *state);
+
+Your function hook_func_incoming does not match this signature, you should adopt it.
+*/
+
 unsigned int main_hook(
-    unsigned int hooknum,
-    struct sk_buff **skb,
-    const struct net_device *in,
-    const struct net_device *out,
-    int (*okfn)(struct sk_buff*)
+    void *priv,
+    struct sk_buff *skb,
+    const struct nf_hook_state *state 
 )
 {
+    printk(KERN_INFO "Packet Dropped.");
     return NF_DROP;
 }
 
-int init_module()
+static int __init drop_init(void)
 {
     netfilter_ops_in.hook               =               main_hook;
     netfilter_ops_in.pf                 =               PF_INET;
-    netfilter_ops_in.hooknum            =               NF_IP_PRE_ROUTING;
+    netfilter_ops_in.hooknum            =               NF_INET_PRE_ROUTING;
     netfilter_ops_in.priority           =               NF_IP_PRI_FIRST;
     
     netfilter_ops_out.hook               =               main_hook;
     netfilter_ops_out.pf                 =               PF_INET;
-    netfilter_ops_out.hooknum            =               NF_IP_POST_ROUTING;
+    netfilter_ops_out.hooknum            =               NF_INET_POST_ROUTING;
     netfilter_ops_out.priority           =               NF_IP_PRI_FIRST;
 
     nf_register_hook(&netfilter_ops_in);
@@ -37,8 +62,11 @@ int init_module()
     return 0;
 }
 
-void cleanup_module()
+static void __exit drop_exit(void)
 {
     nf_unregister_hook(&netfilter_ops_in);
     nf_unregister_hook(&netfilter_ops_out);
 }
+
+module_init(drop_init);
+module_exit(drop_exit);
